32cd9b296350ad6e5f268d2192b157e9157d4386
habsfanhak <56047585+habsfanhak@users.noreply.github.com>
2024-03-11 18:22:52 -0400

    user schema and user register complete

--- server.js
+++ server.js
@@ -2,14 +2,21 @@ const express = require('express')
 const app = express()
 const dotenv = require("dotenv");
 dotenv.config();
+const cors = require('cors');
 const userService = require('./userService.js')
 
-
+app.use(cors());
+app.use(express.json());
 
 const HTTP_PORT = process.env.PORT || 8080;
 
-app.get("/", (req, res) => {
-    res.send("Hi")
+app.post("/register", (req, res) => {
+    userService.registerRegUser(req.body)
+    .then((msg) => {
+        res.json({ "message": msg });
+    }).catch((msg) => {
+        res.status(422).json({ "message": msg });
+    });
 })
 
 userService.connect()
--- userService.js
+++ userService.js
@@ -1,6 +1,24 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 let mongoDBConnectionString = process.env.MONGO_URL;
+
+let Schema = mongoose.Schema;
+
+//Schemas
+let userSchema = new Schema({
+    email: {
+        type: String,
+        unique: true
+    },
+    fullName: String,
+    password: String,
+    phoneNumber: Number,
+    admin: Boolean,
+    authadmin: Boolean
+}, { collection: 'users' });
+
+let User;
 
 module.exports.connect = function () {
     return new Promise(function (resolve, reject) {
@@ -12,7 +30,35 @@ module.exports.connect = function () {
 
         db.once('open', () => {
             console.log("Connected to DB instance.")
+            User = db.model("users", userSchema);
             resolve();
         });
     });
 };
+
+module.exports.registerRegUser = function (userData) {
+    return new Promise(function (resolve, reject) {
+        bcrypt.hash(userData.password, 10).then(hash => {
+
+            userData.password = hash;
+            
+            User.create(
+            {
+                "email" : userData.email,
+                "fullName" : userData.fullName,
+                "password" : userData.password,
+                "phoneNumber" : userData.phoneNumber,
+                "admin" : false,
+                "authadmin" : false
+            }
+            )
+                .then((data) => {
+                resolve(data);
+                })
+                .catch((err) => {
+                reject(`Unable to update. Error: ${err}`);
+                });
+            }).catch((err_ => {
+            }));       
+        });
+}
